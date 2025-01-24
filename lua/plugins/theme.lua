27a044f54ee9aab314df759a8fba3df3f1d971fd
Danil Kostromin <danil.kostromin@icloud.com>
2025-01-24 20:11:30 +0300

    change theme to vesper
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,22 @@
 return {
-  "Shatur/neovim-ayu",
-  name = "ayu-theme",
+  "datsfilipe/vesper.nvim",
+  name = "vesper",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("ayu").setup({
-      -- ...
+    require("vesper").setup({
+      transparent = false, -- Boolean: Sets the background to transparent
+      italics = {
+        comments = false, -- Boolean: Italicizes comments
+        keywords = false, -- Boolean: Italicizes keywords
+        functions = false, -- Boolean: Italicizes functions
+        strings = false, -- Boolean: Italicizes strings
+        variables = false, -- Boolean: Italicizes variables
+      },
+      overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+      palette_overrides = {}, -- ...
     })
 
-    vim.cmd("colorscheme ayu-mirage")
+    vim.cmd("colorscheme vesper")
   end,
 }
